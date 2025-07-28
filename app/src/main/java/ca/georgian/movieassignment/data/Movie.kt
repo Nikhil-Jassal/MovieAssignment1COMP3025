50bfd1714b929f5c75ba3d5332fca7412b7d0a63
Nikhil-Jassal <200566092@student.georgianc.on.ca>
2025-07-28 00:10:44 -0400

    movie class added

--- app/src/main/java/ca/georgian/movieassignment/data/Movie.kt
+++ app/src/main/java/ca/georgian/movieassignment/data/Movie.kt
@@ -1,5 +1,17 @@
 package ca.georgian.movieassignment.data
 
-class Movie
-{
-}
+data class Movie(
+    val Title: String,
+    val Year: String,
+    val Rated: String,
+    val Released: String,
+    val Runtime: String,
+    val Genre: String,
+    val Director: String,
+    val Writer: String,
+    val Actors: String,
+    val Plot: String,
+    val Language: String,
+    val Poster: String,
+    val imdbID: String
+)
